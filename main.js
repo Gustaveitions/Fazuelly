11b4c056a5eb0c5b2db80956cf910e0b55af7ef0
Gustaveitions <134710576+Gustaveitions@users.noreply.github.com>
2024-03-05 09:12:38 -0300

    v

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ alert('comiu o cu de quem tá lendo');
 let nomeUsuario = "";
 let texto = document.querySelector('#texto');
 
-while(nomeUsuario == "" || nomeUsuario == null){
+while(nomeUsuario == ""){
     nomeUsuario = prompt('Qual o tamanho da peça?');
 }
 
