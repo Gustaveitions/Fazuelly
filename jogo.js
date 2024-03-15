56747284e727c6b9a6fcf0ab458f44daf899d432
Gustaveitions <134710576+Gustaveitions@users.noreply.github.com>
2024-03-15 11:43:51 -0300

    nome

---
+++ jogo.js
@@ -0,0 +1,18 @@
+let numeroSecreto = 8;
+
+let chute = prompt('Você já traiu');
+
+
+
+
+while(chute == ''){
+
+}
+
+if(numeroSecreto==chute){
+    alert('100% fiel')
+    console.log('parabéns,acertou!');
+} else{
+    alert('Eu errei fui muluque');
+    console.log('0% File, espero que seja tarido também');
+}
